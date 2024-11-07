5553aed7b27c240a590190fb20e31af05fd3d457
kkarii06 <kkarii06@DESKTOP-QRJDCVI>
2024-11-07 20:32:13 +0200

    Добавьте файлы проекта.

---
+++ Vector3DConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Vector3DLibrary;
+
+namespace Vector3DConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3D v1 = new Vector3D(3, 4, 5);
+            Vector3D v2 = new Vector3D(1, 2, 3);
+
+            Console.WriteLine($"v1: {v1}");
+            Console.WriteLine($"v2: {v2}");
+
+            // Додавання
+            Vector3D sum = v1 + v2;
+            Console.WriteLine($"v1 + v2 = {sum}");
+
+            // Віднімання
+            Vector3D difference = v1 - v2;
+            Console.WriteLine($"v1 - v2 = {difference}");
+
+            // Множення на число
+            Vector3D scaled = v1 * 2;
+            Console.WriteLine($"v1 * 2 = {scaled}");
+
+            // Скалярний добуток
+            double dotProduct = v1 * v2;
+            Console.WriteLine($"v1 * v2 (скалярний добуток) = {dotProduct}");
+
+            // Перевірка рівності
+            Console.WriteLine($"v1 == v2? {v1 == v2}");
+        }
+    }
+}
---
+++ Vector3DLibrary/Vector3D.cs
@@ -0,0 +1,79 @@
+﻿namespace Vector3DLibrary
+{
+    public class Vector3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector3D() { }
+
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введіть координати вектора:");
+            Console.Write("X: ");
+            X = double.Parse(Console.ReadLine());
+            Console.Write("Y: ");
+            Y = double.Parse(Console.ReadLine());
+            Console.Write("Z: ");
+            Z = double.Parse(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return $"Vector3D({X}, {Y}, {Z})";
+        }
+
+        public static Vector3D operator +(Vector3D v1, Vector3D v2)
+        {
+            return new Vector3D(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        public static Vector3D operator -(Vector3D v1, Vector3D v2)
+        {
+            return new Vector3D(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        public static Vector3D operator *(Vector3D v, double scalar)
+        {
+            return new Vector3D(v.X * scalar, v.Y * scalar, v.Z * scalar);
+        }
+
+        public static double operator *(Vector3D v1, Vector3D v2)
+        {
+            return (v1.X * v2.X) + (v1.Y * v2.Y) + (v1.Z * v2.Z);
+        }
+
+        public static bool operator ==(Vector3D v1, Vector3D v2)
+        {
+            return v1.X == v2.X && v1.Y == v2.Y && v1.Z == v2.Z;
+        }
+
+        public static bool operator !=(Vector3D v1, Vector3D v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public static bool operator >(Vector3D v1, Vector3D v2)
+        {
+            return v1.Magnitude() > v2.Magnitude();
+        }
+
+        public static bool operator <(Vector3D v1, Vector3D v2)
+        {
+            return v1.Magnitude() < v2.Magnitude();
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+    }
+}
